e64a391a1ccea55402a570aab7a0ea450e606114
Precious Michael <precious.michael2002@gmail.com>
2026-03-16 21:33:55 +0100

    add: Arithmetic exercise

---
+++ Arithmetic.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        Scanner inputCollector = new Scanner(System.in);
+
+        System.out.println("Enter the first Integer");
+        int firstNumber = inputCollector.nextInt();
+
+        System.out.println("Enter the second Integer");
+        int secondNumber = inputCollector.nextInt();
+
+        int firstNumberSquared = firstNumber * firstNumber;
+        int secondNumberSquared = secondNumber * secondNumber;
+
+        int squareSum = firstNumberSquared + secondNumberSquared;
+        int squareDifference = firstNumberSquared - secondNumberSquared;
+
+        System.out.printf("The squares of the integers %d and %d are %d and %d respectively. The sum of their squares is %d and the difference of the squares is %d",
+                firstNumber, secondNumber, firstNumberSquared, secondNumberSquared, squareSum, squareDifference);
+    }
+}
