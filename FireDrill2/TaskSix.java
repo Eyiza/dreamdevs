cc8b48af82d630960d4b759a1fa7c04ec45eb1b7
Precious Michael <precious.michael2002@gmail.com>
2026-03-17 14:23:01 +0100

    add: second fire drill exercises

---
+++ FireDrill2/TaskSix.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TaskSix {
+    public static void main(String[] args) {
+        Scanner inputCollector = new Scanner(System.in);
+        
+        int sum = 0;
+	int usedNumbers = 0;
+
+	for (int i = 1; i <= 10; i++) {  
+        	System.out.printf("Enter Number %d:%n", i);
+        	int input = inputCollector.nextInt();
+		if (input % 2 == 0) {
+			sum = sum + input;
+			usedNumbers++;
+		}
+	}
+	int average = sum / usedNumbers;
+        System.out.printf("The average of the even numbers is %d", average);
+    }
+}
---
+++ FireDrill2/TaskThree.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TaskThree {
+    public static void main(String[] args) {
+        Scanner inputCollector = new Scanner(System.in);
+        
+        int sum = 0;
+
+	for (int i = 1; i <= 10; i++) {  
+        	System.out.printf("Enter Number %d:%n", i);
+        	int input = inputCollector.nextInt();
+		sum = sum + input; 
+	}
+	int average = sum / 10;
+        System.out.printf("The sum is %d and the average is %d", sum, average);
+    }
+}
