97f7b6bade030dea4fca4fd393bf371085d28eaa
Precious Michael <precious.michael2002@gmail.com>
2026-03-20 22:18:12 +0100

    submit: Back To Sender Logistics

--- src/Weekend1/Logistics.java
+++ src/Weekend1/Logistics.java
@@ -1,4 +1,18 @@
 package Weekend1;
 
 public class Logistics {
+    static final int BASE_PAY = 5000;
+
+    public static int calculateDailyWage(int successfulDelivery){
+        int amountPerParcel = 0;
+
+        if (successfulDelivery < 0) {System.out.printf("%d is invalid%n", successfulDelivery); return 0;}
+        else if (successfulDelivery < 50) amountPerParcel = 160;
+        else if (successfulDelivery < 59) amountPerParcel = 200;
+        else if (successfulDelivery < 69) amountPerParcel = 250;
+        else if (successfulDelivery <= 100) amountPerParcel = 500;
+        else {System.out.printf("%d is invalid%n", successfulDelivery); return 0;}
+
+        return (successfulDelivery * amountPerParcel) + BASE_PAY;
+    }
 }
--- test/Weekend1/LogisticsTest.java
+++ test/Weekend1/LogisticsTest.java
@@ -1,4 +1,37 @@
 package Weekend1;
+import org.junit.jupiter.api.Test;
+
+import static Weekend1.Logistics.calculateDailyWage;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LogisticsTest {
+    @Test
+    public void invalidSuccessDelivery_isZeroTest(){
+        assertEquals(0, calculateDailyWage(-1));
+    }
+
+    @Test
+    public void SuccessDeliveryLessThan50_isValidAllowanceTest(){
+        assertEquals(9000, calculateDailyWage(25));
+    }
+
+    @Test
+    public void SuccessDeliveryLessThan59_isValidAllowanceTest(){
+        assertEquals(16000, calculateDailyWage(55));
+    }
+
+    @Test
+    public void SuccessDeliveryLessThan69_isValidAllowanceTest(){
+        assertEquals(21250, calculateDailyWage(65));
+    }
+
+    @Test
+    public void SuccessDeliveryGreaterThan70_isValidAllowanceTest(){
+        assertEquals(45000, calculateDailyWage(80));
+    }
+
+    @Test
+    public void SuccessDeliveryGreaterThan100_isInvalidTest(){
+        assertEquals(0, calculateDailyWage(104));
+    }
 }
