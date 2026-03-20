7da7069674754d68be045fc882cf74d9f50593cb
Precious Michael <precious.michael2002@gmail.com>
2026-03-20 23:02:05 +0100

    submit: isEven function

--- src/Weekend1/Kata.java
+++ src/Weekend1/Kata.java
@@ -1,4 +1,8 @@
 package Weekend1;
 
 public class Kata {
+    public static boolean isEven(int number) {
+        if (number % 2 == 0) return true;
+        else return false;
+    }
 }
--- test/Weekend1/KataTest.java
+++ test/Weekend1/KataTest.java
@@ -1,4 +1,15 @@
 package Weekend1;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class KataTest {
+    @Test
+    public void sixIsEvenTest() {
+        assertTrue(Kata.isEven(6));
+    }
+
+    @Test
+    public void fiveIsEvenTest() {
+        assertFalse(Kata.isEven(5));
+    }
 }
