a5ac1967ab570fd78fddab42fa7c00c6f3992366
Precious Michael <precious.michael2002@gmail.com>
2026-03-21 11:11:14 +0100

    submit: maximumIn function

--- src/Weekend1/ArrayKata.java
+++ src/Weekend1/ArrayKata.java
@@ -1,4 +1,11 @@
 package Weekend1;
 
 public class ArrayKata {
+    public static int maximumIn(int[] numbers) {
+        int maximum = numbers[0];
+        for (int number : numbers) {
+            if (number > maximum) maximum = number;
+        }
+        return maximum;
+    }
 }
--- test/Weekend1/ArrayKataTest.java
+++ test/Weekend1/ArrayKataTest.java
@@ -1,4 +1,20 @@
 package Weekend1;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static Weekend1.ArrayKata.*;
 
 public class ArrayKataTest {
+    int[] numbers;
+
+    @Test
+    public void largestNumberInArray_Is9Test() {
+        numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertEquals(9, maximumIn(numbers));
+    }
+
+    @Test
+    public void largestNumberInArray_Is20Test() {
+        numbers = new int[] {20, 5, 10};
+        assertEquals(20, maximumIn(numbers));
+    }
 }
