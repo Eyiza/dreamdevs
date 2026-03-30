adc6626b23a5ea50bb93246d7155c0c1c8961b1d
Precious Michael <precious.michael2002@gmail.com>
2026-03-30 10:05:32 +0100

    add: sliding window dsa problem tests

--- test/dsa/MaxSubArrayTest.java
+++ test/dsa/MaxSubArrayTest.java
@@ -8,16 +8,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class MaxSubArrayTest {
 
     @Test
-    void maxSubArrayTest() {
+    void maxSubArray_kIs3Test() {
         int[] array = {1,5,6,9,3,2};
         int[] result = {5,6,9};
         assertArrayEquals(result,maxSubArray(array,3));
     }
 
     @Test
-    void maxSubArray_Test() {
+    void maxSubArray_InRearrangedArray_kIs3Test() {
         int[] array = {10,5,6,9,3,2};
         int[] result = {10,5,6};
         assertArrayEquals(result,maxSubArray(array,3));
+    }
+
+    @Test
+    void maxSubArray_kIs4Test() {
+        int[] array = {1,5,6,9,3,2};
+        int[] result = {5,6,9,3};
+        assertArrayEquals(result,maxSubArray(array,4));
+    }
+
+    @Test
+    void maxSubArray_InRearrangedArray_kIs4Test() {
+        int[] array = {10,5,6,9,3,2};
+        int[] result = {10,5,6,9};
+        assertArrayEquals(result,maxSubArray(array,4));
     }
 }
